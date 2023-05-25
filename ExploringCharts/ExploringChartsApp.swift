11cfa191a334794cdd6f8819338c15b259b38a35
Flavio Vinicius Campos <flavioviniciuscampos@Flavios-MacBook-Pro.local>
2023-05-25 09:10:13 -0300

    First commit

---
+++ ExploringCharts/ExploringChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploringChartsApp.swift
+//  ExploringCharts
+//
+//  Created by Flavio Vinicius Campos on 25/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ExploringChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
